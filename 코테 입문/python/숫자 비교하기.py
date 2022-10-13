629a874d7c15bc6b67f395674491d2a936251631
t1mmy_t1m <leechaechae2006@gmail.com>
2022-10-13 22:51:05 +0900

    Create 숫자 비교하기.py

---
+++ 코테 입문/python/숫자 비교하기.py
@@ -0,0 +1,7 @@
+def solution(num1, num2):
+    if 0 <= num1 <= 10000 and 0 <= num2 <= 10000:
+        if num1 == num2:
+            answer = 1
+        elif num1 != num2:
+            answer = -1
+    return answer
